acc0b84e0b3e1ecb8d7bf182ecce2bc6045de3ec
Dmitrii-Ivanov30 <dmitrii.ivanov@tul.cz>
2022-10-06 08:56:19 +0200

    druhy commit

---
+++ src/Cviceni/cv01.java
@@ -0,0 +1,7 @@
+package Cviceni;
+
+public class cv01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Prednasky/ALG_P2.java
@@ -0,0 +1,9 @@
+package Prednasky;
+
+public class ALG_P2 {
+    public static void main(String[] args) {
+        int k =5;
+        int l = k++;
+        System.out.println(k + " " + l);
+    }
+}
